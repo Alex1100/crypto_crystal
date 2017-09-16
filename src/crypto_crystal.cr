5c2de1f2161017a92a9cd2ca2da09675edfc805b
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-09-16 00:12:55 -0700

    add kemal server/routes and http ssl requests

--- src/crypto_crystal.cr
+++ src/crypto_crystal.cr
@@ -1,5 +1,47 @@
 require "./crypto_crystal/*"
+require "http/client"
+require "http/server"
+require "json"
+require "time/span"
+require "uri"
+require "kemal"
 
 module CryptoCrystal
-  # TODO Put your code here
+  before_get "/" do |env|
+    env.set "livecoin_BTCUSD", 0
+    env.set "cCex_BTCUSD", 0
+  end
+
+  get "/" do |env|
+    livecoin_BTCUSD = env.get "livecoin_BTCUSD"
+    cCex_BTCUSD = env.get "cCex_BTCUSD"
+    render "src/views/layout.ecr"
+  end
+
+  before_get "/prices" do |env|
+    livecoin_res = JSON.parse((HTTP::Client.get("https://api.livecoin.net/exchange/ticker").body))[2]
+    livecoin_BTCUSD = livecoin_res["last"]
+    cCex_res = JSON.parse((HTTP::Client.get("https://c-cex.com/t/prices.json").body))
+    cCex_BTCUSD = cCex_res["btc-usd"]["lastprice"]
+    env.set "livecoin_BTCUSD", livecoin_BTCUSD.to_s
+    env.set "cCex_BTCUSD", cCex_BTCUSD.to_s
+  end
+
+  get "/prices" do |env|
+    livecoin_BTCUSD = env.get "livecoin_BTCUSD"
+    cCex_BTCUSD = env.get "cCex_BTCUSD"
+    render "src/views/layout.ecr"
+  end
+
+  # get "/prices.json" do |env|
+  #   livecoin_res = JSON.parse((HTTP::Client.get("https://api.livecoin.net/exchange/ticker").body))[2]
+  #   cCex_res = JSON.parse((HTTP::Client.get("https://c-cex.com/t/prices.json").body))
+  #   cCex_BTCUSD = cCex_res["usd-btc"]
+  #   req = [livecoin_res, cCex_BTCUSD]
+  #   prices = {livecoin_BTCUSD: req[0], cCex_BTCUSD: req[1]}.to_json
+  #   env.response.content_type = "application/json"
+  #   prices
+  # end
+
+  Kemal.run
 end

87650f4d457cc60c3a90d0967a90a62977940857
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-09-18 15:45:24 -0700

    add ticker css changes and hitbtc

--- public/js/src/containers/App.jsx
+++ public/js/src/containers/App.jsx
@@ -5,15 +5,225 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      livecoin: [0,0,0,0,0,0,0],
-      cCex: [0,0,0,0,0,0,0]
+      livecoin: ["0","0","0","0","0","0","0"],
+      cCex: ["0","0","0","0","0","0","0"],
+      hitbtc: ["0","0","0","0","0","0","0"],
+      prev_livecoin: ["0","0","0","0","0","0","0"],
+      prev_cCex: ["0","0","0","0","0","0","0"],
+      prev_hitbtc: ["0","0","0","0","0","0","0"]
     }
 
     this.fetchPrices = this.fetchPrices.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount(){
     this.fetchPrices();
+  }
+
+  handleChange(){
+    const { prev_cCex, prev_livecoin, prev_hitbtc } = this.state;
+
+    if(prev_livecoin[0] === "0"){
+      return;
+    }
+
+    const cCex = document.getElementsByClassName('cCex');
+    const Livecoin = document.getElementsByClassName('livecoin');
+    const HitBTC = document.getElementsByClassName('hitbtc');
+
+    if(cCex[0].innerHTML !== prev_cCex[0]){
+      if(parseFloat(cCex[0].innerHTML).toPrecision(5) > parseFloat(prev_cCex[0]).toPrecision(5)){
+        cCex[0].style.webkitTextStroke = "1px green"
+      } else {
+        cCex[0].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      cCex[0].style.webkitTextStroke = ""
+    }
+    if(cCex[1].innerHTML !== prev_cCex[1]){
+      if(parseFloat(cCex[1].innerHTML).toPrecision(5) > parseFloat(prev_cCex).toPrecision(5)){
+        cCex[1].style.webkitTextStroke = "1px green"
+      } else {
+        cCex[1].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      cCex[1].style.webkitTextStroke = ""
+    }
+    if(cCex[2].innerHTML !== prev_cCex[2]){
+      if(parseFloat(cCex[2].innerHTML).toPrecision(5) > parseFloat(prev_cCex[2]).toPrecision(5)){
+        cCex[2].style.webkitTextStroke = "1px green"
+      } else {
+        cCex[2].style.webkitTextStroke = "1px red";
+      }
+    } else {
+      cCex[2].style.webkitTextStroke = ""
+    }
+    if(cCex[3].innerHTML !== prev_cCex[3]){
+      if(parseFloat(cCex[3].innerHTML).toPrecision(5) > parseFloat(prev_cCex[3]).toPrecision(5)){
+        cCex[3].style.webkitTextStroke = "1px green"
+      } else {
+        cCex[3].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      cCex[3].style.webkitTextStroke = ""
+    }
+    if(cCex[4].innerHTML !== prev_cCex[4]){
+      if(parseFloat(cCex[4].innerHTML).toPrecision(5) > parseFloat(prev_cCex).toPrecision(5)){
+        cCex[4].style.webkitTextStroke = "1px green"
+      } else {
+        cCex[4].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      cCex[4].style.webkitTextStroke = ""
+    }
+    if(cCex[5].innerHTML !== prev_cCex[5]){
+      if(parseFloat(cCex[5].innerHTML).toPrecision(5) > parseFloat(prev_cCex[5]).toPrecision(5)){
+        cCex[5].style.webkitTextStroke = "1px green"
+      } else {
+        cCex[5].style.webkitTextStroke = "1px red";
+      }
+    } else {
+      cCex[5].style.webkitTextStroke = ""
+    }
+    if(cCex[6].innerHTML !== prev_cCex[6]){
+      if(parseFloat(cCex[6].innerHTML).toPrecision(5) > parseFloat(prev_cCex).toPrecision(5)){
+        cCex[6].style.webkitTextStroke = "1px green"
+      } else {
+        cCex[6].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      cCex[6].style.webkitTextStroke = ""
+    }
+
+    if(Livecoin[0].innerHTML !== prev_livecoin[0]){
+      if(parseFloat(Livecoin[0].innerHTML).toPrecision(5) > parseFloat(prev_livecoin[0]).toPrecision(5)){
+        Livecoin[0].style.webkitTextStroke = "1px green"
+      } else {
+        Livecoin[0].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      Livecoin[0].style.webkitTextStroke = ""
+    }
+    if(Livecoin[1].innerHTML !== prev_livecoin[1]){
+      if(parseFloat(Livecoin[1].innerHTML).toPrecision(5) > parseFloat(prev_livecoin).toPrecision(5)){
+        Livecoin[1].style.webkitTextStroke = "1px green"
+      } else {
+        Livecoin[1].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      Livecoin[1].style.webkitTextStroke = ""
+    }
+    if(Livecoin[2].innerHTML !== prev_livecoin[2]){
+      if(parseFloat(Livecoin[2].innerHTML).toPrecision(5) > parseFloat(prev_livecoin[2]).toPrecision(5)){
+        Livecoin[2].style.webkitTextStroke = "1px green"
+      } else {
+        Livecoin[2].style.webkitTextStroke = "1px red";
+      }
+    } else {
+      Livecoin[2].style.webkitTextStroke = ""
+    }
+    if(Livecoin[3].innerHTML !== prev_livecoin[3]){
+      if(parseFloat(Livecoin[3].innerHTML).toPrecision(5) > parseFloat(prev_livecoin[3]).toPrecision(5)){
+        Livecoin[3].style.webkitTextStroke = "1px green"
+      } else {
+        Livecoin[3].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      Livecoin[3].style.webkitTextStroke = ""
+    }
+    if(Livecoin[4].innerHTML !== prev_livecoin[4]){
+      if(parseFloat(Livecoin[4].innerHTML).toPrecision(5) > parseFloat(prev_livecoin).toPrecision(5)){
+        Livecoin[4].style.webkitTextStroke = "1px green"
+      } else {
+        Livecoin[4].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      Livecoin[4].style.webkitTextStroke = ""
+    }
+    if(Livecoin[5].innerHTML !== prev_livecoin[5]){
+      if(parseFloat(Livecoin[5].innerHTML).toPrecision(5) > parseFloat(prev_livecoin[5]).toPrecision(5)){
+        Livecoin[5].style.webkitTextStroke = "1px green"
+      } else {
+        Livecoin[5].style.webkitTextStroke = "1px red";
+      }
+    } else {
+      Livecoin[5].style.webkitTextStroke = ""
+    }
+    if(Livecoin[6].innerHTML !== prev_livecoin[6]){
+      if(parseFloat(Livecoin[6].innerHTML).toPrecision(5) > parseFloat(prev_livecoin).toPrecision(5)){
+        Livecoin[6].style.webkitTextStroke = "1px green"
+      } else {
+        Livecoin[6].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      Livecoin[6].style.webkitTextStroke = ""
+    }
+
+
+    if(HitBTC[0].innerHTML !== prev_hitbtc[0]){
+      if(parseFloat(HitBTC[0].innerHTML).toPrecision(5) > parseFloat(prev_hitbtc[0]).toPrecision(5)){
+        HitBTC[0].style.webkitTextStroke = "1px green"
+      } else {
+        HitBTC[0].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      HitBTC[0].style.webkitTextStroke = ""
+    }
+    if(HitBTC[1].innerHTML !== prev_hitbtc[1]){
+      if(parseFloat(HitBTC[1].innerHTML).toPrecision(5) > parseFloat(prev_hitbtc).toPrecision(5)){
+        HitBTC[1].style.webkitTextStroke = "1px green"
+      } else {
+        HitBTC[1].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      HitBTC[1].style.webkitTextStroke = ""
+    }
+    if(HitBTC[2].innerHTML !== prev_hitbtc[2]){
+      if(parseFloat(HitBTC[2].innerHTML).toPrecision(5) > parseFloat(prev_hitbtc[2]).toPrecision(5)){
+        HitBTC[2].style.webkitTextStroke = "1px green"
+      } else {
+        HitBTC[2].style.webkitTextStroke = "1px red";
+      }
+    } else {
+      HitBTC[2].style.webkitTextStroke = ""
+    }
+    if(HitBTC[3].innerHTML !== prev_hitbtc[3]){
+      if(parseFloat(HitBTC[3].innerHTML).toPrecision(5) > parseFloat(prev_hitbtc[3]).toPrecision(5)){
+        HitBTC[3].style.webkitTextStroke = "1px green"
+      } else {
+        HitBTC[3].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      HitBTC[3].style.webkitTextStroke = ""
+    }
+    if(HitBTC[4].innerHTML !== prev_hitbtc[4]){
+      if(parseFloat(HitBTC[4].innerHTML).toPrecision(5) > parseFloat(prev_hitbtc).toPrecision(5)){
+        HitBTC[4].style.webkitTextStroke = "1px green"
+      } else {
+        HitBTC[4].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      HitBTC[4].style.webkitTextStroke = ""
+    }
+    if(HitBTC[5].innerHTML !== prev_hitbtc[5]){
+      if(parseFloat(HitBTC[5].innerHTML).toPrecision(5) > parseFloat(prev_hitbtc[5]).toPrecision(5)){
+        HitBTC[5].style.webkitTextStroke = "1px green"
+      } else {
+        HitBTC[5].style.webkitTextStroke = "1px red";
+      }
+    } else {
+      HitBTC[5].style.webkitTextStroke = ""
+    }
+    if(HitBTC[6].innerHTML !== prev_hitbtc[6]){
+      if(parseFloat(HitBTC[6].innerHTML).toPrecision(5) > parseFloat(prev_hitbtc).toPrecision(5)){
+        HitBTC[6].style.webkitTextStroke = "1px green"
+      } else {
+        HitBTC[6].style.webkitTextStroke = "1px red"
+      }
+    } else {
+      HitBTC[6].style.webkitTextStroke = ""
+    }
   }
 
 
@@ -22,25 +232,37 @@ class App extends Component{
       axios.get('/prices.json')
       .then(res => {
         this.setState({
+          prev_livecoin: this.state.livecoin,
+          prev_cCex: this.state.cCex,
+          prev_hitbtc: this.state.hitbtc,
           livecoin: [
-            res.data.livecoin.BTCUSD.last,
-            res.data.livecoin.ETHUSD.last,
-            res.data.livecoin.ETHBTC.last,
-            res.data.livecoin.LTCUSD.last,
-            res.data.livecoin.LTCBTC.last,
-            res.data.livecoin.DASHUSD.last,
-            res.data.livecoin.DASHBTC.last
+            parseFloat(res.data.livecoin.BTCUSD).toPrecision(5),
+            parseFloat(res.data.livecoin.ETHUSD).toPrecision(5),
+            parseFloat(res.data.livecoin.ETHBTC).toPrecision(5),
+            parseFloat(res.data.livecoin.LTCUSD).toPrecision(5),
+            parseFloat(res.data.livecoin.LTCBTC).toPrecision(5),
+            parseFloat(res.data.livecoin.DASHUSD).toPrecision(5),
+            parseFloat(res.data.livecoin.DASHBTC).toPrecision(5)
           ],
           cCex: [
-            res.data.cCex.BTCUSD.lastprice,
-            res.data.cCex.ETHUSD.lastprice,
-            res.data.cCex.ETHBTC.lastprice,
-            res.data.cCex.LTCUSD.lastprice,
-            res.data.cCex.LTCBTC.lastprice,
-            res.data.cCex.DASHUSD.lastprice,
-            res.data.cCex.DASHBTC.lastprice
+            parseFloat(res.data.cCex.BTCUSD).toPrecision(5),
+            parseFloat(res.data.cCex.ETHUSD).toPrecision(5),
+            parseFloat(res.data.cCex.ETHBTC).toPrecision(5),
+            parseFloat(res.data.cCex.LTCUSD).toPrecision(5),
+            parseFloat(res.data.cCex.LTCBTC).toPrecision(5),
+            parseFloat(res.data.cCex.DASHUSD).toPrecision(5),
+            parseFloat(res.data.cCex.DASHBTC).toPrecision(5)
+          ],
+          hitbtc: [
+            parseFloat(res.data.hitbtc.BTCUSD).toPrecision(5),
+            parseFloat(res.data.hitbtc.ETHUSD).toPrecision(5),
+            parseFloat(res.data.hitbtc.ETHBTC).toPrecision(5),
+            parseFloat(res.data.hitbtc.LTCUSD).toPrecision(5),
+            parseFloat(res.data.hitbtc.LTCBTC).toPrecision(5),
+            parseFloat(res.data.hitbtc.DASHUSD).toPrecision(5),
+            parseFloat(res.data.hitbtc.DASHBTC).toPrecision(5)
           ]
-        });
+        }, () => this.handleChange());
       })
       .catch(err => {
         console.log("ERR GETTING PRICES FROM CRYSTAL: ", err);
@@ -49,12 +271,12 @@ class App extends Component{
   }
 
   render(){
-    const { cCex, livecoin } = this.state;
+    const { cCex, livecoin, hitbtc } = this.state;
     return(
       <div>
         <div className="ticker-container">
           <h1>React on Crystal CryptoCurrency Ticker</h1>
-          <h1>C-Cex/Livecoin CryptoCurrency Prices</h1>
+          <h1>C-Cex/Livecoin/HitBTC CryptoCurrency Prices</h1>
           <table className="c-cex-table">
             <tr>
               <th>C-Cex BTCUSD</th>
@@ -66,13 +288,13 @@ class App extends Component{
               <th>C-Cex DASHBTC</th>
             </tr>
             <tr>
-              <td>{ cCex[0] }</td>
-              <td>{ cCex[1] }</td>
-              <td>{ cCex[2] }</td>
-              <td>{ cCex[3] }</td>
-              <td>{ cCex[4] }</td>
-              <td>{ cCex[5] }</td>
-              <td>{ cCex[6] }</td>
+              <td className="cCex">{ cCex[0] }</td>
+              <td className="cCex">{ cCex[1] }</td>
+              <td className="cCex">{ cCex[2] }</td>
+              <td className="cCex">{ cCex[3] }</td>
+              <td className="cCex">{ cCex[4] }</td>
+              <td className="cCex">{ cCex[5] }</td>
+              <td className="cCex">{ cCex[6] }</td>
             </tr>
           </table>
           <br/>
@@ -88,13 +310,35 @@ class App extends Component{
               <th>Livecoin DASHBTC</th>
             </tr>
             <tr>
-              <td>{ livecoin[0] }</td>
-              <td>{ livecoin[1] }</td>
-              <td>{ livecoin[2] }</td>
-              <td>{ livecoin[3] }</td>
-              <td>{ livecoin[4] }</td>
-              <td>{ livecoin[5] }</td>
-              <td>{ livecoin[6] }</td>
+              <td className="livecoin">{ livecoin[0] }</td>
+              <td className="livecoin">{ livecoin[1] }</td>
+              <td className="livecoin">{ livecoin[2] }</td>
+              <td className="livecoin">{ livecoin[3] }</td>
+              <td className="livecoin">{ livecoin[4] }</td>
+              <td className="livecoin">{ livecoin[5] }</td>
+              <td className="livecoin">{ livecoin[6] }</td>
+            </tr>
+          </table>
+          <br/>
+          <br/>
+          <table className="hitbtc-table">
+            <tr>
+              <th>HitBTC BTCUSD</th>
+              <th>HitBTC ETHUSD</th>
+              <th>HitBTC ETHBTC</th>
+              <th>HitBTC LTCUSD</th>
+              <th>HitBTC LTCBTC</th>
+              <th>HitBTC DASHUSD</th>
+              <th>HitBTC DASHBTC</th>
+            </tr>
+            <tr>
+              <td className="hitbtc">{ hitbtc[0] }</td>
+              <td className="hitbtc">{ hitbtc[1] }</td>
+              <td className="hitbtc">{ hitbtc[2] }</td>
+              <td className="hitbtc">{ hitbtc[3] }</td>
+              <td className="hitbtc">{ hitbtc[4] }</td>
+              <td className="hitbtc">{ hitbtc[5] }</td>
+              <td className="hitbtc">{ hitbtc[6] }</td>
             </tr>
           </table>
           <br/>
--- src/crypto_crystal.cr
+++ src/crypto_crystal.cr
@@ -11,28 +11,39 @@ module CryptoCrystal
   get "/prices.json" do |env|
     livecoin_res = JSON.parse((HTTP::Client.get("https://api.livecoin.net/exchange/ticker").body))
     cCex_res = JSON.parse((HTTP::Client.get("https://c-cex.com/t/prices.json").body))
+    hitbtc_res = JSON.parse((HTTP::Client.get("https://api.hitbtc.com/api/1/public/ticker").body))
+
+    hitbtc = {
+      "BTCUSD":  hitbtc_res["BTCUSD"]["last"],
+      "ETHUSD":  hitbtc_res["ETHUSD"]["last"],
+      "ETHBTC":  hitbtc_res["ETHBTC"]["last"],
+      "LTCUSD":  hitbtc_res["LTCUSD"]["last"],
+      "LTCBTC":  hitbtc_res["LTCBTC"]["last"],
+      "DASHUSD": hitbtc_res["DASHUSD"]["last"],
+      "DASHBTC": hitbtc_res["DASHBTC"]["last"],
+    }
 
     livecoin = {
-      "BTCUSD":  livecoin_res[2],
-      "ETHUSD":  livecoin_res[23],
-      "ETHBTC":  livecoin_res[24],
-      "LTCUSD":  livecoin_res[6],
-      "LTCBTC":  livecoin_res[7],
-      "DASHUSD": livecoin_res[18],
-      "DASHBTC": livecoin_res[19],
+      "BTCUSD":  livecoin_res[2]["last"],
+      "ETHUSD":  livecoin_res[23]["last"],
+      "ETHBTC":  livecoin_res[24]["last"],
+      "LTCUSD":  livecoin_res[6]["last"],
+      "LTCBTC":  livecoin_res[7]["last"],
+      "DASHUSD": livecoin_res[18]["last"],
+      "DASHBTC": livecoin_res[19]["last"],
     }
 
     cCex = {
-      "BTCUSD":  cCex_res["btc-usd"],
-      "ETHUSD":  cCex_res["eth-usd"],
-      "ETHBTC":  cCex_res["eth-btc"],
-      "LTCUSD":  cCex_res["ltc-usd"],
-      "LTCBTC":  cCex_res["ltc-btc"],
-      "DASHUSD": cCex_res["dash-usd"],
-      "DASHBTC": cCex_res["dash-btc"],
+      "BTCUSD":  cCex_res["btc-usd"]["lastprice"],
+      "ETHUSD":  cCex_res["eth-usd"]["lastprice"],
+      "ETHBTC":  cCex_res["eth-btc"]["lastprice"],
+      "LTCUSD":  cCex_res["ltc-usd"]["lastprice"],
+      "LTCBTC":  cCex_res["ltc-btc"]["lastprice"],
+      "DASHUSD": cCex_res["dash-usd"]["lastprice"],
+      "DASHBTC": cCex_res["dash-btc"]["lastprice"],
     }
 
-    prices = {livecoin: livecoin, cCex: cCex}.to_json
+    prices = {livecoin: livecoin, cCex: cCex, hitbtc: hitbtc}.to_json
     env.response.content_type = "application/json"
     prices
   end
